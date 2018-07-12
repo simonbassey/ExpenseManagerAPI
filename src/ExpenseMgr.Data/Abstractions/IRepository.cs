66464413b50312f6877938f1e99b6d381189e2a6
simonbassey <simon.dev.bassey@gmail.com>
2018-07-12 20:31:14 +0100

    Added  a repository abstraction

--- src/ExpenseMgr.Data/Abstractions/IRepository.cs
+++ src/ExpenseMgr.Data/Abstractions/IRepository.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace ExpenseMgr.Data.Abstractions
 {
-    public class IRepository
+    public interface IRepository<T> where T : class
     {
-        public IRepository()
-        {
-        }
+        Task<T> AddAsync(T tEntity);
+        Task<T> GetAsync(params object[] key);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FilterAsync(Func<T, bool> criteria);
     }
 }
