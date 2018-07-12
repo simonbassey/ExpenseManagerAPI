cf2714714ee17c23ee075a186a04e4082fe32f55
simonbassey <simon.dev.bassey@gmail.com>
2018-07-12 16:10:14 +0100

    Initialized DomainEntity project

---
+++ src/ExpenseMrg.Domain/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using ExpenseMgr.Domain;
+
+namespace ExpenseMrg.Domain
+{
+    public class Expense
+    {
+        [Key]
+        public int ExpenseId { get; set; }
+        public Guid UserId { get; set; }
+        public double Amount { get; set; }
+        public double Description { get; set; }
+
+
+        public virtual User User { get; set; }
+
+    }
+}
---
+++ src/ExpenseMrg.Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace ExpenseMgr.Domain
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
