5c388a36cffef7fc1ef9fd5c87d507307f972a50
simonbassey <simon.dev.bassey@gmail.com>
2018-07-12 20:32:44 +0100

    Added User and Expense Repository - contract and Implemenation

--- src/ExpenseMgr.Data/Repositories/ExpenseRepository.cs
+++ src/ExpenseMgr.Data/Repositories/ExpenseRepository.cs
@@ -1,10 +1,64 @@
 ﻿using System;
+using ExpenseMrg.Domain;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Linq;
+
 namespace ExpenseMgr.Data.Repositories
 {
-    public class ExpenseRepository
+    public interface IExpenseRepository
+    {
+        Task<Expense> SaveExpense(Expense newExpense);
+        Task<Expense> GetExpense(int expenseId);
+        Task<IEnumerable<Expense>> GetExpenses();
+        Task<IEnumerable<Expense>> FilterExpensis(Func<Expense, bool> searchQuery);
+        Task<bool> ExpenseExists(string title, DateTime date);
+    }
+
+    public class ExpenseRepository : BaseRepository<Expense>, IExpenseRepository
     {
         public ExpenseRepository()
         {
+            //we leave here an empty constructor for dependency injection
+        }
+
+        public async Task<IEnumerable<Expense>> FilterExpensis(Func<Expense, bool> searchQuery)
+        {
+            return await FilterAsync(searchQuery);
+        }
+
+        public async Task<Expense> GetExpense(int expenseId)
+        {
+            return await GetAsync(expenseId);
+        }
+
+        public async Task<IEnumerable<Expense>> GetExpenses()
+        {
+            return await GetAllAsync();
+        }
+
+        public async Task<Expense> SaveExpense(Expense newExpense)
+        {
+            try
+            {
+                var expenseRecordExist = await ExpenseExists(newExpense.Title, newExpense.ExpenseDate.Value);
+                if (expenseRecordExist)
+                    return null;
+                return await AddAsync(newExpense);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ExpenseExists(string title, DateTime date)
+        {
+            return (await FilterAsync(exp => exp.Title.Equals(title, StringComparison.OrdinalIgnoreCase) &&
+                                      (exp.ExpenseDate - date)?.TotalMinutes < 1)).FirstOrDefault() != null;
         }
     }
 }
--- src/ExpenseMgr.Data/Repositories/UserRepository.cs
+++ src/ExpenseMgr.Data/Repositories/UserRepository.cs
@@ -1,10 +1,49 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseMgr.Domain;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace ExpenseMgr.Data.Repositories
 {
-    public class UserRepository
+    public interface IUserRepository
+    {
+        Task<User> CreateUser(User user);
+        Task<User> GetUser(string key);
+        Task<IEnumerable<User>> Search(Func<User, bool> searchQuery);
+    }
+
+    public class UserRepository : BaseRepository<User>, IUserRepository
     {
         public UserRepository()
         {
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            var userExists = await UserExist(user.Email);
+            if (userExists)
+                return null;
+            return await AddAsync(user);
+        }
+
+        public async Task<User> GetUser(string key)
+        {
+            return await GetAsync(key);
+        }
+
+        public async Task<IEnumerable<User>> Search(Func<User, bool> searchQuery)
+        {
+            return await FilterAsync(searchQuery);
+        }
+
+        private async Task<bool> UserExist(string userId)
+        {
+            return (await FilterAsync(user => user.Email.Equals(userId, StringComparison.OrdinalIgnoreCase) ||
+                                      user.UserName.Equals(userId, StringComparison.OrdinalIgnoreCase))).FirstOrDefault() != null;
+
         }
     }
 }
