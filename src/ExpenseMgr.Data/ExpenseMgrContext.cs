a483684a93e7627f63d9f7fa4694f5b9860da286
simonbassey <simon.dev.bassey@gmail.com>
2018-07-12 20:29:18 +0100

    Added Entity framework for Object relational mapping

--- src/ExpenseMgr.Data/ExpenseMgrContext.cs
+++ src/ExpenseMgr.Data/ExpenseMgrContext.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using ExpenseMrg.Domain;
+using ExpenseMgr.Domain;
 namespace ExpenseMgr.Data
 {
-    public class ExpenseMgrContext
+    public class ExpenseMgrContext : DbContext
     {
-        public ExpenseMgrContext()
+        public ExpenseMgrContext() { }
+        public ExpenseMgrContext(DbContextOptions options) : base(options)
         {
+
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ExpenseMgr.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<Expense> Expenses { get; set; }
+        public virtual DbSet<User> Users { get; set; }
     }
 }
