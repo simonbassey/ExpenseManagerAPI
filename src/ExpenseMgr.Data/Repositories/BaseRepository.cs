bdd3b85697350de6e375840c31d6e08a4dc97f2c
simonbassey <simon.dev.bassey@gmail.com>
2018-07-12 20:32:01 +0100

    Added Generic Base Repository Implementation

--- src/ExpenseMgr.Data/Repositories/BaseRepository.cs
+++ src/ExpenseMgr.Data/Repositories/BaseRepository.cs
@@ -1,10 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ExpenseMgr.Data.Abstractions;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 namespace ExpenseMgr.Data.Repositories
 {
-    public class BaseRepository
+    public abstract class BaseRepository<T> : IRepository<T> where T : class
     {
-        public BaseRepository()
+        public virtual async Task<T> AddAsync(T tEntity)
         {
+            using (var db = new ExpenseMgrContext())
+            {
+                await db.Set<T>().AddAsync(tEntity);
+                return (await db.SaveChangesAsync()) == 1 ? tEntity : null;
+            }
+        }
+
+        public virtual async Task<IEnumerable<T>> FilterAsync(Func<T, bool> criteria)
+        {
+            using (var db = new ExpenseMgrContext())
+            {
+                return await db.Set<T>().Where(criteria).AsQueryable().ToListAsync();
+            };
+
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            using (var db = new ExpenseMgrContext())
+            {
+                return await db.Set<T>().ToListAsync();
+            }
+        }
+
+        public virtual async Task<T> GetAsync(params object[] key)
+        {
+            using (var db = new ExpenseMgrContext())
+            {
+                return await db.Set<T>().FindAsync(key);
+            }
         }
     }
 }
