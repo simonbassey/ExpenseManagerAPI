13f4e75f6aa3a300351684cf69e61ee51c94d975
simonbassey <simon.dev.bassey@gmail.com>
2018-07-19 14:44:40 +0100

    Removed sensitive data from configuration

--- src/ExpenseMgr.Data/ExpenseMgrContext.cs
+++ src/ExpenseMgr.Data/ExpenseMgrContext.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using ExpenseMrg.Domain;
 using ExpenseMgr.Domain;
 using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Diagnostics;
 namespace ExpenseMgr.Data
 {
     public class ExpenseMgrContext : DbContext
@@ -33,9 +35,15 @@ namespace ExpenseMgr.Data
         private string GetConnectionString()
         {
             IConfiguration configuration = StaticServiceResolver.Resolve<IConfiguration>();
-            return configuration.GetConnectionString("trackyaexpenseDbConnection");
+            var connectionString = string.Empty;
+#if DEBUG
+            connectionString = configuration.GetConnectionString("trackyaexpenseDbConnection");
+#elif !DEBUG
+            connectionString =  configuration["trackyaexpenseDbConnection"];
+#endif
+            return connectionString;
         }
-        #endregion
 
+        #endregion
     }
 }
